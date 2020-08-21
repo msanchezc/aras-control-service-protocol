b875818b8b7f7b15d9e93a5024773baf34b265c8
Carlos Tovar <cartovarc@gmail.com>
2020-08-21 12:12:38 -0500

    fix

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
+requirements = []
+
 with open("requirements.txt", "r") as fh:
     requirements = fh.readlines()
-
-requirements = []
 
 setup_requirements = ['pytest-runner']
 
