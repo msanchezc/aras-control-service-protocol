8895198cefacb362e20c15f4ff8249a900765fd6
Carlos Tovar <cartovarc@gmail.com>
2020-08-21 11:49:58 -0500

    fix

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/cartovarc/aras_control_service_protocol',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
